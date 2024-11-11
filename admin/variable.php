b046213d5f1eb3d7b853ddf0e51c1313ec2e222c
LagunaKc22 <choygggg58@gmail.com>
2024-11-11 22:28:09 +0800

    added new variable for end shift and break

--- admin/variable.php
+++ admin/variable.php
@@ -11,16 +11,42 @@ $fnow =  date('m/d/Y, g:i:A');
 
 $intime =  date('Y-m-d H:i:s');
 $bstime =  date('Y-m-d H:i:s');
-$betime =  date('Y-m-d H:i:s');
-$outtime =  date('Y-m-d H:i:s');
+$betime =  date( 'Y-m-d H:i:s');
+$outtime =  date( 'Y-m-d H:i:s');
+$fullname = $fname . " " . $lname;
+
+$shiftDuration = new DateTime($intime);
+$shiftDuration->modify('+9 hours');
+
+$breakDuration = new DateTime($bstime);
+$breakDuration->modify('+1 hour');
+
+
+$finalShift = $shiftDuration->format('h:i A');
+$finalBreak = $breakDuration->format('h:i A');
+
+echo "End of Break Time " . $finalBreak. "<br><br>"; 
+echo "End of the shift " . $finalShift. "<br><br>"; 
+//IT CAN NOW GET THE EXACT END TIME OF SHIFT AND BREAK
+
+
+
+
+
+
 
 // Display current date and user information (optional)
-echo "Date: ". $ftoday;
-echo "<br>";
-echo "Now ".$fnow;
-echo "<br><br>";
-echo "User ID: " .$id;
-echo "<br>";
-$fullname = $fname . " " . $lname;
-echo "User Full Name: " .$fullname;
-echo "<br><br>";?>
+// echo "Date: ". $ftoday;
+// echo "<br>";
+// echo "Now ".$fnow;
+// echo "<br><br>";
+// echo "User ID: " .$id;
+// echo "<br>";
+
+// echo "User Full Name: " .$fullname;
+// echo "<br><br>";
+
+
+
+
+?>
